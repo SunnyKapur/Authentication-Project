143de25ba83920ed3a6cd8ffe92bdc6ab20fd84d
SunnyKapur <kumarsunnykumar678@gmail.com>
2026-05-28 16:14:15 +0530

    add delete note controller with error handling

--- auth-api-v1/src/controllers/note.controllers.js
+++ auth-api-v1/src/controllers/note.controllers.js
@@ -92,3 +92,26 @@ export let updateNoteController = async (req, res) => {
     });
   }
 };
+
+export let deleteNoteController = async (req, res) => {
+  try {
+    
+    let {id} = req.params
+
+    if(!id) return res.status(404).json({
+        message: "Id note found"
+    })
+
+    await NoteModel.findByIdAndDelete(id)
+
+    return res.status(200).json({
+        message: "note deleted successfully"
+    })
+
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
--- auth-api-v1/src/routes/notes.routes.js
+++ auth-api-v1/src/routes/notes.routes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { createNoteController, getAllNoteController, updateNoteController } from "../controllers/note.controllers.js";
+import {
+  createNoteController,
+  deleteNoteController,
+  getAllNoteController,
+  updateNoteController,
+} from "../controllers/note.controllers.js";
 
 let router = express.Router();
 
 router.post("/create", createNoteController);
 router.get("/", getAllNoteController);
-router.patch('/update/:id', updateNoteController)
+router.patch("/update/:id", updateNoteController);
+router.delete("/delete/:id", deleteNoteController);
 
 export default router;
