56f854837a832cdc4b8b09ea6926c38ae6c7bedd
SunnyKapur <kumarsunnykumar678@gmail.com>
2026-05-28 15:13:46 +0530

    note model and schema created

---
+++ auth-api-v1/src/models/note.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+let noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+let NoteModel = mongoose.model("notes", noteSchema)
+
+export default NoteModel
